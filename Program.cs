4749658958639b8c8e85e24d358c63bda535c599
Molodkina Nelly <molodkinanelli@gmail.com>
2023-03-26 14:01:30 +0300

    Создали метод для выполнения условия задачи

--- Program.cs
+++ Program.cs
@@ -6,3 +6,16 @@
 string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
 string[] array2 = new string[array1.Length];
 
+// Создаем метод для выполнения условия задачи
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
